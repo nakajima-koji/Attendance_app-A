b5b983ccb84a2736ec98b3ac1702d5f702c16e83
nakajimakoji <crazy.max.meteora@gmail.com>
2020-02-07 08:10:32 +0000

    layout修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def full_title(page_name = "")
     base_title = "AttendanceApp"
-    if page_name.empty?
+    if page_name.empty?  #empty?はあるのが前提
       base_title
     else
       page_name + " | " + base_title
