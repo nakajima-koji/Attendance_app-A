940431fb7006b6baadf53bf88a04f81b361f5084
nakajimakoji <crazy.max.meteora@gmail.com>
2020-02-10 12:10:45 +0000

    10.4 add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
+  before_action :set_one_month, only: [:show]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -13,6 +14,8 @@ class UsersController < ApplicationController
   end
   
   def show
+    @first_day = Date.current.beginning_of_month
+    @last_day = @first_day.end_of_month
   end
   
   def create
